47d1c0925e518ed5616225f8d73304c573b1a998
Kto-to_123 <nikolai.sv13@gmail.com>
2020-08-04 23:47:23 +0300

    add asset

---
+++ Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    Vector3 cameraPosition;
+    [SerializeField] float t = 0.1f;
+
+    private void Awake()
+    {
+        cameraPosition = transform.position - player.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position + cameraPosition, t);
+    }
+}
---
+++ Assets/TapController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapController : MonoBehaviour
+{
+    [SerializeField] PathCreation.Examples.PathFollower player;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            player.speed = 5f;
+        }
+        else
+        {
+            player.speed = 0f;
+        }
+    }
+}
